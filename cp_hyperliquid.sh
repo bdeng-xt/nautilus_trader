d33fc598892550027290884b2e0b3434bc04bc23
trader0 <xt>
2025-08-28 13:28:50 +0000

    update hyperliquid instrument providers

---
+++ cp_hyperliquid.sh
@@ -0,0 +1,5 @@
+cp docs/integrations/hyperliquid.md ~/Documents/src/nautilus_trader/docs/integrations/
+cp -r crates/adapters/hyperliquid/* ~/Documents/src/nautilus_trader/crates/adapters/hyperliquid/
+cp -r nautilus_trader/adapters/hyperliquid/* ~/Documents/src/nautilus_trader/nautilus_trader/adapters/hyperliquid/
+cp Cargo.lock ~/Documents/src/nautilus_trader/
+cp -r examples/live/hyperliquid/* ~/Documents/src/nautilus_trader/examples/live/hyperliquid/ 
